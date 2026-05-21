4da26cdab1ee3503d57aad158a5e27192df940f8
Adriano Mesquita <adriano87339286@gmail.com>
2026-05-21 15:28:07 -0300

    logout

--- src/main.js
+++ src/main.js
@@ -3046,8 +3046,7 @@ async function initConfiguracoes() {
             localStorage.removeItem('token');
             localStorage.removeItem('usuario');
 
-            window.location.replace("./index.html");
-
+            window.location.href = "/demonstracao-sistema-ti/index.html";
         };
     }
 
@@ -3056,7 +3055,7 @@ async function initConfiguracoes() {
         const token = localStorage.getItem("token");
 
         if (!token && window.location.pathname.includes("index-sistema")) {
-            window.location.href = "./index.html";
+            window.location.href = "/demonstracao-sistema-ti/index.html";
         }
     });
 
